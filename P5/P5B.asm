231a3b83eef115ea7432b608e9b020c4045f8b60
S3ba5ti4n <sebastian.diaz48@uabc.edu.mx>
2025-10-09 11:22:11 -0700

    Update P5B.asm

--- P5/P5B.asm
+++ P5/P5B.asm
@@ -2,22 +2,25 @@
 								; que se encuentran en la biblioteca libpc_io.a
 
 section	.text
-	global _start       ;referencia para inicio de programa
+	global _start       ; referencia para inicio de programa
 	
-_start:                   
+_start:
+	; Imprimir cadena original
 	mov edx, msg		; edx = dirección de la cadena msg
-	call puts			; imprime cadena msg terminada en valor nulo (0)
+	call puts			; imprime cadena original
 
-    mov dl,'X'
-    mov ebx,msg+23
-    mov byte [ebx],dl 
-    call puts
-    
-    mov edx, msg        ; edx = direccion de la cadena msg
-    call puts           ; imprime cadena msg terminada en valor nulo (0)
+	; Reemplazar la letra 'x' (posición 23) con 'X' usando direccionamiento indirecto
+	mov dl, 'X'
+	mov ebx, msg + 23   ; ebx apunta al carácter 'x'
+	mov byte [ebx], dl  ; almacena 'X' en la posición apuntada por ebx
 
+	; Imprimir cadena modificada
+	mov edx, msg
+	call puts
+
+	; Terminar programa
 	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
 	int	0x80        	; llamada al sistema - fin de programa
 
 section	.data
-msg	db  'abcdefghijklmnopqrstuvwxyz0123456789',10,0 
+msg	db  'abcdefghijklmnopqrstuvwxyz0123456789', 10, 0
