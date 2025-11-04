8ebcfc23d542f2f4a68b78ad3c69f32e27795fa9
S3ba5ti4n <sebastian.diaz48@uabc.edu.mx>
2025-11-04 19:36:09 +0000

    P9

--- P9/P9.asm
+++ P9/P9.asm
@@ -3,56 +3,114 @@
 ;nasm -f elf64 P9.asm -o P9.o
 	;ld	-m	elf_i386	P9.o	../LIB/libpc_iox.a	-o	P9.asm
 ;./P9
-section	.text
+global _start
+ extern read_char, print_char, print_hex_byte, print_nl, print_string
+ section .data
+ msgA db "Ingrese vector A:",0
+ msgB db "Ingrese vector B:",0  
+ msgS db "Suma de vectores (hex):",0
+ msgP db "Producto escalar (hex):",0
+ N equ 5
+ vecA times N db 0
+ vecB times N db 0
 
-	global _start       ;must be declared for using gcc
+ section .text
+
+captura_vector:
+    push eax
+    push ecx
+    push ebx
+leer_dato:
+    call read_char
+    sub al, '0'
+    cmp al, 9
+    jae leer_dato
+    mov [ebx], al
+    inc ebx
+    loop leer_dato
+    pop ebx
+    pop ecx
+    pop eax
+    ret
+
+desplegar_vector_hex:
+    push eax
+    push ebx
+    push ecx
+mostrar:
+    mov al, [ebx]
+    call print_hex_byte
+    mov al, ' '
+    call print_char
+    inc ebx
+    loop mostrar
+    call print_nl
+    pop ecx
+    pop ebx
+    pop eax
+    ret
+
+sumar_vectores:
+    push eax
+    push ebx
+    push edx
+    push ecx
+sumar:
+    mov al, [ebx]
+    add al, [edx]
+    mov [ebx], al
+    inc ebx
+    inc edx
+    loop sumar
+    pop ecx
+    pop edx
+    pop ebx
+    pop eax
+    ret
 
 _start:
+    mov ebx, msgA
+    call print_string
+    mov ebx, vecA
+    mov ecx, N
+    call captura_vector
 
- Capturar_Vector:
- call getche
+    mov ebx, msgB
+    call print_string
+    mov ebx, vecB
+    mov ecx, N
+    call captura_vector
 
- cmp al, '10'
- jl, es_menor
+    mov ebx, vecA
+    mov edx, vecB
+    mov ecx, N
+    call sumar_vectores
 
+    mov ebx, msgS
+    call print_string
+    mov ebx, vecA
+    mov ecx, N
+    call desplegar_vector_hex
 
-es_menor:
- mov EBX, no_registrto_N
+    mov ebx, vecA
+    mov edx, vecB
+    mov ecx, N
+    xor esi, esi
+producto:
+    mov al, [ebx]
+    mov bl, [edx]
+    imul bl
+    add esi, eax
+    inc ebx
+    inc edx
+    loop producto
 
-Despliegue_de_vector:
+    mov ebx, msgP
+    call print_string
+    mov al, sil
+    call print_hex_byte
+    call print_nl
 
- mov ebx,
- mov ecx 
- mov, cx
-
-Calc_Sum_Vectores:
- 
- mov vector, 
- mov edx
-
- ecx sum, vector
-
-
-
-
-Prog_lee_almacena_vectores:
-es_letra prgo 
-q alcna, ecx
-sto, ecx
-(c+ 1) (c+2)
-
-
-.loop_presentacion:
-    ; Cargar el caracter desde el arreglo
-    mov al, [esi]
-    call putc           ; Imprimir el caracter
-
- call putc
- inc esi
-
- loop .loop_presentacion 
-
-    
-    ; FIN del programa
-    
-    call exit 
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
